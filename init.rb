585b4c9d63aed4352a7146eb1d5ae6fe7c046d8b
Paul <tacid@tacid.kiev.ua>
2016-06-16 03:37:42 +0400

    version bump 1.0.0
    just a common used version number

--- init.rb
+++ init.rb
@@ -3,7 +3,7 @@ Redmine::Plugin.register :zip_attachments do
   name 'Zip Attachments plugin'
   author 'Brian Mwadime'
   description 'A redmine plugin to enable downloading of issue attachments as a single zip.'
-  version '1'
+  version '1.0.0'
   url 'https://github.com/brianmwadime/redmine-zip-attachments'
   author_url 'http://brianmwadime.com'
 
